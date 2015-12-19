6b26a81b4ba3604bf3b0cc69756e799c4c0060d1
Flug <flugv1@gmail.com>
2015-12-19 18:05:58 +0100

    fix cs

--- src/Stitch/Stitch.php
+++ src/Stitch/Stitch.php
@@ -13,104 +13,104 @@ use SplFileObject;
 class Stitch extends SplFileObject
 {
 
-        /**
-         *
-         * The method for open file
-         *
-         */
-        const   OPEN_READ_ONLY = "r",
-            OPEN_READ_WRITE_PLUS = "r+",
-            OPEN_WRITE_ONLY_CREATE = "w",
-            OPEN_WRITE_READ_CREATE = "w+",
-            OPEN_READ_ONLY_END_FILE = "a",
-            OPEN_READ_WRITE_END_FILE = "a+",
-            OPEN_CREATE_WRITE = "x",
-            OPEN_CREATE_WRITE_READ = "x+",
-            OPEN_WRITE_ONLY_CREATE_UNTRUNCATE = "c",
-            OPEN_WRITE_READ_CREATE_UNTRUNCATE = "c+";
-        /**
-         * Name of file serialize
-         * @var string
-         *
-         */
-        private $filename;
-        private $settings = [
-            'csv' => [
-                "delimiter" => ';',
-                "enclosure" => '"',
-                "escape" => '"'
-            ]
-        ];
+    /**
+     *
+     * The method for open file
+     *
+     */
+    const   OPEN_READ_ONLY = "r",
+        OPEN_READ_WRITE_PLUS = "r+",
+        OPEN_WRITE_ONLY_CREATE = "w",
+        OPEN_WRITE_READ_CREATE = "w+",
+        OPEN_READ_ONLY_END_FILE = "a",
+        OPEN_READ_WRITE_END_FILE = "a+",
+        OPEN_CREATE_WRITE = "x",
+        OPEN_CREATE_WRITE_READ = "x+",
+        OPEN_WRITE_ONLY_CREATE_UNTRUNCATE = "c",
+        OPEN_WRITE_READ_CREATE_UNTRUNCATE = "c+";
+    /**
+     * Name of file serialize
+     * @var string
+     *
+     */
+    private $filename;
+    private $settings = [
+        'csv' => [
+            "delimiter" => ';',
+            "enclosure" => '"',
+            "escape" => '"'
+        ]
+    ];
 
-        ////////////////////////////////////////////////////////////////////////
-        ////////////////////////////Constructor/////////////////////////////////
-        ////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+    ////////////////////////////Constructor/////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
 
-        /**
-         *
-         * Create a new Stitch instance.
-         *
-         * @param string $filename
-         * @param self Constante|string $openStreamType
-         * @param array $settings
-         */
-        public function __construct($filename, $openStreamType = null, $settings = [])
-        {
+    /**
+     *
+     * Create a new Stitch instance.
+     *
+     * @param string $filename
+     * @param self Constante|string $openStreamType
+     * @param array $settings
+     */
+    public function __construct($filename, $openStreamType = null, $settings = [])
+    {
 
-                if ($openStreamType == null) {
-                        $openStreamType = self::OPEN_READ_ONLY_END_FILE;
-                }
-
-                if ((bool)$settings) {
-                        $this->settings = $settings;
-                }
-
-                parent::__construct($filename, $openStreamType);
-                parent::setCsvControl(
-                    $this->settings['csv']['delimiter'],
-                    $this->settings['csv']['enclosure'],
-                    $this->settings['csv']['escape']
-                );
-                $this->filename = $filename;
+        if ($openStreamType == null) {
+            $openStreamType = self::OPEN_READ_ONLY_END_FILE;
         }
 
-        public static function  instance($filename, $openStreamType = null, $settings = [])
-        {
-                return new static ($filename, $openStreamType , $settings);
+        if ((bool)$settings) {
+            $this->settings = $settings;
         }
 
-        public function addHeader(array $inputHeader = [])
-        {
-                return self::fputcsv(
-                    $inputHeader,
-                    $this->settings['csv']['delimiter'],
-                    $this->settings['csv']['enclosure']
-                );
+        parent::__construct($filename, $openStreamType);
+        parent::setCsvControl(
+            $this->settings['csv']['delimiter'],
+            $this->settings['csv']['enclosure'],
+            $this->settings['csv']['escape']
+        );
+        $this->filename = $filename;
+    }
+
+    public static function  instance($filename, $openStreamType = null, $settings = [])
+    {
+        return new static ($filename, $openStreamType, $settings);
+    }
+
+    public function addHeader(array $inputHeader = [])
+    {
+        return self::fputcsv(
+            $inputHeader,
+            $this->settings['csv']['delimiter'],
+            $this->settings['csv']['enclosure']
+        );
+    }
+
+    public function writeCollection($collection = [])
+    {
+
+        foreach ($collection as $lineItems) {
+            self::writeContentLine($lineItems);
         }
+    }
 
-        public function writeCollection($collection = [])
-        {
+    public function writeContentLine(array  $lineItems = [])
+    {
+        return self::fputcsv($lineItems,
+            $this->settings['csv']['delimiter'],
+            $this->settings['csv']['enclosure']
 
-                foreach ($collection as $lineItems) {
-                        self::writeContentLine($lineItems);
-                }
+        );
+    }
+
+    public function remove()
+    {
+        if (file_exists($this->filename)) {
+            return unlink($this->filename);
         }
-
-        public function writeContentLine(array  $lineItems = [])
-        {
-                return self::fputcsv($lineItems,
-                    $this->settings['csv']['delimiter'],
-                    $this->settings['csv']['enclosure']
-
-                );
-        }
-
-        public function remove()
-        {
-                if (file_exists($this->filename)) {
-                        return unlink($this->filename);
-                }
-                return false;
-        }
+        return false;
+    }
 }
 
